de85c0549efc7b3ff84785d35ef80672045425fc
arunkramar <arunr@onwardpath.com>
2020-06-02 21:44:23 -0500

    used PostMapping instead of GetMapping for signup url

--- src/main/java/com/onwardpath/wem/controller/UserController.java
+++ src/main/java/com/onwardpath/wem/controller/UserController.java
@@ -2,12 +2,13 @@ package com.onwardpath.wem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
 	
-	@GetMapping("/signup")
+	@PostMapping("/signup")
 	public String signup() {
 		
 		return "";
